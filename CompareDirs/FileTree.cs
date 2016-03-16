bed84afb813f1ec1fbf33fc1bbc446a4d0e804ad
Matt <matt.spooner10@gmail.com>
2016-03-16 09:07:43 -0400

    Initial Commit

---
+++ CompareDirs/FileTree.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompareDirs {
+	class TreeRoot {
+		//members
+		private FileTree root;
+		//constructors
+		internal TreeRoot(string rootFile) {
+			root = new FileTree(Directory.GetFileSystemEntries(rootFile), new FileInfo(rootFile).Name);
+		}
+		//methods
+		public void Print() {
+			root.Print("");
+		}
+	}
+	class FileTree : Traversable{
+		//members
+		private string folderName;
+		private Traversable[] children;
+		//constructors
+		internal FileTree(string[] fileNames, string folderName) {
+			this.folderName = folderName;
+			children = new Traversable[fileNames.Length];
+			for(int i = 0; i < children.Length; i++) {
+				FileInfo fi = new FileInfo(fileNames[i]);
+				if(fi.Attributes.HasFlag(FileAttributes.Directory)) {
+					children[i] = new FileTree(Directory.GetFileSystemEntries(fileNames[i]), fi.Name);
+				}
+				else {
+					children[i] = new TreeItem(fi.Name);
+				}
+				Console.WriteLine(fi.Extension);
+			}
+		}
+		//methods
+		public void Print(string tabs) {
+			Console.WriteLine(tabs + folderName);
+			tabs += "\t";
+			foreach (Traversable t in children) {
+				t.Print(tabs);
+			}
+		}
+	}
+	class TreeItem : Traversable {
+		//members
+		private string fileName;
+		//constructors
+		internal TreeItem(string fileName) {
+			this.fileName = fileName;
+		}
+		//methods
+		public void Print(string tabs) {
+			Console.WriteLine(tabs + fileName);
+		}
+	}
+	interface Traversable {
+		void Print(string tabs);
+	}
+}
---
+++ CompareDirs/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CompareDirs { 
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window {
+		//members
+		private string path1, path2;
+		//constructors
+		public MainWindow() {
+			InitializeComponent();
+			path1 = path2 = null;
+		}
+		//methods
+		public void OpenChooser1Click(object sender, RoutedEventArgs e) {
+			path1 = OpenDialog();
+		}
+		public void OpenChooser2Click(object sender, RoutedEventArgs e) {
+			path2 = OpenDialog();
+        }
+		public string OpenDialog() {
+			FolderBrowserDialog dialog = new FolderBrowserDialog();
+			dialog.ShowNewFolderButton = false;
+			DialogResult res = dialog.ShowDialog();
+			return res.Equals(System.Windows.Forms.DialogResult.OK) ? dialog.SelectedPath : null;
+		}
+		public void CompareButtonClick(object sender, RoutedEventArgs e) {
+			//try {
+				if (path1 != null && path2 != null) {
+					TreeRoot root1 = new TreeRoot(path1);
+					root1.Print();
+					TreeRoot root2 = new TreeRoot(path2);
+					root2.Print();
+				}
+			//}
+			//catch(Exception x) {
+			//Console.WriteLine(x.Message);
+			//}
+		}
+	}
+}
