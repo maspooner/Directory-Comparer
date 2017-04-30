d72032c9700462a07b442a880f7baa078c3914ca
Matthew <spoonerm12@gmail.com>
2017-04-30 16:42:24 -0400

    Added Costura.Fody dependancy to integreate the Ookii Dialogs into the exe

--- CompareDirs/Properties/AssemblyInfo.cs
+++ CompareDirs/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Spoon Labs")]
 [assembly: AssemblyProduct("CompareDirs")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,7 +51,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.1")]
+[assembly: AssemblyFileVersion("2.0.0.1")]
 [assembly: NeutralResourcesLanguage("en")]
 
